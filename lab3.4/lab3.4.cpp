fb575be921d7a0f9e5fb34189e048cea948f70f0
X13X666X <newmicrogamer@gmail.com>
2024-10-28 09:33:56 +0200

    upload

---
+++ lab3.4/lab3.4.cpp
@@ -0,0 +1,34 @@
+﻿// Lab_03_4.cpp
+// < Чапля Максим >
+// Лабораторна робота № 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 34
+
+#include <iostream>
+#include <cmath> 
+
+using namespace std;
+
+int main() {
+    double x, y, R1, R2;
+
+    cout << "x = "; cin >> x;
+    cout << "y = "; cin >> y;
+    cout << "R1 = "; cin >> R1;
+    cout << "R2 = "; cin >> R2;
+
+    double distanceSquared = x * x + y * y;
+
+    bool A = (distanceSquared >= pow(R1, 2) && distanceSquared <= pow(R2, 2) && x > 0 && y > 0);
+    bool B = (distanceSquared >= pow(R1, 2) && distanceSquared <= pow(R2, 2) && x < 0 && y < 0);
+
+    if ((distanceSquared >= R1 * R1 && distanceSquared <= R2 * R2) &&
+        ((x > 0 && y > 0) || (x < 0 && y < 0))) {
+        cout << "yes" << endl;
+    }
+    else {
+        cout << "no" << endl;
+    }
+
+    return 0;
+}
